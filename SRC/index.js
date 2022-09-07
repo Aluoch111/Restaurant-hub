04943333361ede39956ef7cbb0707be6fd31f85b
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-08 01:29:55 +0300

    inial commit

--- SRC/index.js
+++ SRC/index.js
@@ -10,16 +10,18 @@
    alert("Thank you for subscribing.")
    });
    
-
-   const options = {
-     method: 'GET',
-     headers: {
-        'X-RapidAPI-Key': '1c5476fc26mshed3810bef19629bp1c44ecjsn5eaf3d529c49',
-        'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
-     }
-  };
-  
-  fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156', options)
-     .then(response => response.json())
-     .then(response => console.log(response))
-     .catch(err => console.error(err));
+function fetchData(){
+    const options = {
+        method: 'GET',
+        headers: {
+           'X-RapidAPI-Key': '1c5476fc26mshed3810bef19629bp1c44ecjsn5eaf3d529c49',
+           'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
+        }
+     };
+     
+     fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+  fetchData();
