4cee0caf94ad033a2173f8b42f9493e3ed307ee7
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-08 16:35:33 +0300

    add comment section

--- SRC/index.js
+++ SRC/index.js
@@ -14,6 +14,23 @@
    let click = document.getElementById("api-data");
    click.addEventListener("click",fetchData );
    
+   let form = document.querySelector("form");
+   form.addEventListener("submit", (e) => {
+
+      e.preventDefault();
+
+       const input = document.getElementById("input");
+       const comments = document.getElementById("ul");
+       const newComment = document.createElement("li");
+       newComment.innerHTML = input.value;
+       comments.appendChild(newComment);
+   });
+
+ 
+      
+
+
+
 
 
    
@@ -32,14 +49,16 @@ function fetchData(){
             console.log(data.data);
             const items = data.data .map( api=> {
                 return `
+
                  <div class = "menu">
 
                     <p> <img src = "${api.mainPhotoSrc}"  height = " 184px"/> </p>
+                    <br>
                     <p> Name : ${api.name}</p>
                     <p> Specials: ${api.servesCuisine}</p>
                     <p> Offers: ${api.bestOffer.name} </p>
                     <p> Price-range:${api.priceRange}</p>
-                    
+                    <br>
                  </div> 
                 `;
             })
@@ -51,3 +70,5 @@ function fetchData(){
         })
         .catch(err => console.error(err));
 }
+
+
